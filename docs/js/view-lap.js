24a2372685dfbfb34c33af4d73ec6805b68fbd51
lvh201998 <52194766+lvh201998@users.noreply.github.com>
2022-01-02 10:18:31 +0700

    m

---
+++ docs/js/view-lap.js
@@ -0,0 +1,98 @@
+setTimeout(function() {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("main-layout").style.display = "block";
+}, 0);
+
+$(".oj24").click(function(event) {
+    event.stopPropagation()
+    console.log("zoom")
+    $(".video-test").addClass("video-test-show")
+    $(".item-hire").addClass("hire-zoom")
+})
+
+$(".oj29").click(function() {
+    console.log("hire")
+    $(".video-test").removeClass("video-test-show")
+    $(".item-hire").removeClass("hire-zoom")
+})
+
+$(".hoanthanh").click(function() {
+    var datl = $(".chon-da");
+    if (datl.length == 0) {
+        Toastify({
+            text: "Hoàn thành câu hỏi",
+            duration: 1000,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            backgroundColor: "red",
+        }).showToast();
+    } else {
+        console.log("hire")
+        $(".video-test").removeClass("video-test-show")
+        $(".item-hire").removeClass("hire-zoom")
+
+        setTimeout(function() {
+            $(".oj28").css("display", "none")
+            $(".oj27").addClass("oj27-zoom")
+        }, 800)
+
+        setTimeout(function() {
+            goEquipment()
+        }, 3000)
+    }
+
+})
+
+$("#xemvideo").click(function(event) {
+    event.stopPropagation()
+    console.log("video")
+    $(".list-cauhoi").removeClass("display-block")
+    $(".video-play").removeClass("display-none")
+    $("#xemvideo").toggleClass("tab-choose")
+    $("#cauhoi").toggleClass("tab-choose")
+})
+
+$("#cauhoi").click(function(event) {
+    event.stopPropagation()
+    tabCauhoi()
+})
+
+function tabCauhoi() {
+    $("#cauhoi").toggleClass("tab-choose")
+    $("#xemvideo").toggleClass("tab-choose")
+    console.log("cauhoi")
+    $(".list-cauhoi").addClass("display-block")
+    $(".video-play").addClass("display-none")
+}
+
+function goEquipment() {
+    $("#main-layout").addClass("layout-opacity-7")
+    $("#loadingGoto").css("display", "flex")
+    setTimeout(function() {
+        window.location.href = 'equipment.html'
+    }, 600)
+}
+
+function chooseDA(da) {
+    console.log("chon da")
+    if (da == 1)
+        $(".oj32").toggleClass("chon-da")
+    if (da == 2)
+        $(".oj33").toggleClass("chon-da")
+    if (da == 3)
+        $(".oj34").toggleClass("chon-da")
+    if (da == 4)
+        $(".oj35").toggleClass("chon-da")
+
+    setTimeout(function() {
+        $(".show-rs").css("opacity", 1);
+    }, 500)
+}
+
+var video = document.getElementsByTagName('video')[0];
+video.onended = function(e) {
+    setTimeout(function() {
+        tabCauhoi()
+    }, 1500)
+};
